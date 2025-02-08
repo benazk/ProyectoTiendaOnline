8169e54c85cdbc0869904c9c11237903490351c1
benazk <benatezquerro128@gmail.com>
2025-02-09 00:58:56 +0100

    bug de stripe arreglado e iconos funcionales

--- mi_app/__init__.py
+++ mi_app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from sqlalchemy.dialects.postgresql import ARRAY
+from flask_compress import Compress
 
 app = Flask(__name__)
+Compress(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.secret_key = 'key_dwes_2023'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://benat:12345678@localhost/TiendaJuegos?port=5432'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://benat:12345678@localhost/tiendajuegos?port=5432'
 db = SQLAlchemy(app)
 from  mi_app.tienda.vistas import tienda
 app.register_blueprint(tienda)
